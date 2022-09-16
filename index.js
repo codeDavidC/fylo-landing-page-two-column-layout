eb220900a1b3b961f327e9f1f86d2c8a3a72580f
David Chavez <email@example.com>
2022-09-16 11:09:07 -0400

    styling for desktop update

---
+++ index.js
@@ -0,0 +1,13 @@
+// https://www.frontendmentor.io/solutions/fylo-landing-page-two-column-hover-state-and-email-validation-b41BpwOgV3
+
+const formTop = document.querySelector(".form-top");
+const formBottom = document.querySelector(".form-bottom");
+
+/* TO PREVENT PAGE REFRESH */
+formTop.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+formBottom.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
